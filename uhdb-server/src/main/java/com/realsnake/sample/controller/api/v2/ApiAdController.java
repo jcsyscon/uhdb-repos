38253dc5fae8b76fffbafcf253daa1465ef1e0bf
dew3737 <dew3377@naver.com>
2018-06-15 18:19:37 +0900

    광고 카테고리 목록 조회, 광고 목록 조회 추가

--- uhdb-server/src/main/java/com/realsnake/sample/controller/api/v2/ApiAdController.java
+++ uhdb-server/src/main/java/com/realsnake/sample/controller/api/v2/ApiAdController.java
@@ -21,6 +21,7 @@ import com.realsnake.sample.model.common.AttachFileVo;
 import com.realsnake.sample.model.common.SimpleAd;
 import com.realsnake.sample.model.common.api.ApiResponse;
 import com.realsnake.sample.service.ad.AdService;
+import com.realsnake.sample.util.MobilePagingHelper;
 
 @RestController("ApiV2AdController")
 @RequestMapping(value = "/api/v2/ad")
@@ -107,6 +108,72 @@ public class ApiAdController {
         }
     }
     
+    /**
+     * 광고 카테고리 목록
+     */
+    @GetMapping(value = "/category/list")
+    public ApiResponse<?> findCategoryList() throws CommonApiException {
+    	try {
+    		 Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
 
+             if (authentication == null) {
+                 LOGGER.debug("<<ApiAdController.getAd>> 인증 실패");
+                 throw new CommonApiException(ApiResultCode.NOTFOUND_USER);
+             }
+             
+    		String[][] categories = {{"01", "Best20"}, {"02", "음식"}, {"03", "학원"}, {"04", "스포츠"}, {"05", "미용/뷰티"}, {"06", "생활편의"}};
 
+            List<Map<String, Object>> categoryList = new ArrayList<>();
+            for (String[] category : categories) {
+                Map<String, Object> categoryMap = new HashMap<>();
+                categoryMap.put("categoryCode", category[0]);
+                categoryMap.put("categoryName", category[1]);
+                categoryList.add(categoryMap);
+            }
+    		
+    		ApiResponse<List<Map<String, Object>>> apiResponse = new ApiResponse<>();
+            apiResponse.setBody(categoryMapList);
+
+            return apiResponse;
+    	} catch (Exception e) {
+            throw new CommonApiException(ApiResultCode.COMMON_FAIL, e);
+        }
+    }
+
+    /**
+     * 광고 목록 조회
+     */
+    @GetMapping(value = "/list")
+    public ApiResponse<?> findAdList(MobilePagingHelper mobilePagingHelper, AdDto param, String categoryCode) throws CommonApiException {
+    	try {
+    		 Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+             if (authentication == null) {
+                 LOGGER.debug("<<ApiAdController.getAd>> 인증 실패");
+                 throw new CommonApiException(ApiResultCode.NOTFOUND_USER);
+             }
+             
+             List<AdCtgrVo> adCtgrList = this.adService.findAdList(param, categoryCode);
+             
+             List<Map<String, Object>> adCtgrList = new ArrayList<>();
+             for (AdCtgrVo adCtgr : adCtgrList) {
+            	 Map<String, Object> adCtgrMap = new HashMap<>();
+            	 adCtgrMap.put("adSeq", adCtgr.getAdSeq());
+            	 adCtgrMap.put("adTitle", adCtgr.getAdTitle());
+            	 adCtgrMap.put("shopName", adCtgr.getShopName());
+            	 adCtgrMap.put("shopTel", adCtgr.getShopTel());
+            	 adCtgrList.add();
+             }
+             
+             Map<String, Object> bodyMap = new HashMap<>();
+             bodyMap.put("adCtgrList", adCtgrList);
+             
+             ApiResponse<Map<String, Object>> apiResponse = new ApiResponse<>(mobilePagingHelper.getNextPageToken(), mobilePagingHelper.getPageSize());
+             apiResponse.setBody(categoryMapList);
+
+             return apiResponse;
+    	} catch (Exception e) {
+            throw new CommonApiException(ApiResultCode.COMMON_FAIL, e);
+        }
+    }
 }
